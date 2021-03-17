c709d36ebb3660f3dd271c0649ed4dd7b0aa5daa
krunal prajapati <krunalcodemaster@gmail.com>
2021-03-17 23:54:17 +0530

    header ui changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ $(function () {
   if ($(".scroll").length) {
     $(".scroll").jScrollPane();
   }
+  $(".hamburgar").on("click", function () {
+    $("nav").slideToggle("open");
+    $("nav").toggleClass("open");
+    $(this).toggleClass("open");
+  });
 });
 
 $(window).resize(function () {
