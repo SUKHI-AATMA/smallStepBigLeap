27c9f5e6fa2416805e8c68638ea8cf6c81ea71c3
Krunal Prajapati <krunal@intellimedianetworks.net>
2021-03-23 01:37:07 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,17 @@ $(function () {
     $("nav").toggleClass("open");
     $(this).toggleClass("open");
   });
+  $("body").on("click", ".listigHeader .compDetails, .action", function (e) {
+    var el = $(this).parents(".listigHeader");
+    $(".box-acco").find(".level-box").stop().slideUp();
+    if ($(el).find(".action").hasClass("active")) {
+      $(el).find(".action").removeClass("active");
+    } else {
+      $(".action").removeClass("active");
+      $(el).find(".action").addClass("active");
+    }
+    $(el).closest(".box-acco").find(".level-box").stop().slideToggle();
+  });
 });
 
 $(window).resize(function () {
