96a132b30e73e3521c5c53270d50117bfb906c80
Rajesh Sawant <sawant1810@gmail.com>
2021-04-22 20:00:36 +0530

    updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,8 +128,24 @@ function animateClass() {
 $(window)
     .on("load", function() {
         animateClass();
+        var secParam = getParameterByName('sec');
+        if(secParam)
+        {
+            var scrlTp = $('.'+secParam).offset().top - $('header').outerHeight(true);
+            $('html,body').animate({scrollTop:scrlTp});
+        }
     })
     .on("scroll", function() {
         animateClass();
     });
 animateClass();
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
